21ddf337957953191d683dde94207dac7455c250
“tatsuki0091” <rossosalvatore@icloud.com>
2020-03-08 19:13:35 +0900

    一覧取得を発売日の降順に設定

--- sneaker_shop_api/urls.py
+++ sneaker_shop_api/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')), # 追加
+    path('product_api/', include('products.urls')), # 追加
 ]
